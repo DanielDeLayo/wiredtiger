92ebbf79a32a9e508a7521ff2aeb42d2d883e413
Daniel DeLayo <dandelayo@gmail.com>
2026-04-30 14:31:40 -0400

    fixing debugging paths

--- src/btree/bt_read.c
+++ src/btree/bt_read.c
@@ -689,7 +689,8 @@ skip_evict:
 
 #ifdef HAVE_ANALYZE_CACHE
 
-    if (!F_ISSET(btree, WT_BTREE_SPECIAL_FLAGS) && !F_ISSET(session, WT_SESSION_INTERNAL)) 
+    // Check if we're in a metadata zone or not.
+    if (!F_ISSET(btree, WT_BTREE_SPECIAL_FLAGS) && !F_ISSET(session, WT_SESSION_INTERNAL | WT_SESSION_CACHE_CURSORS)  ) 
     {
         assert(page->persistent_page_id != IAF_ID_UNINIT && "Uninitialized persistent_page_id!");
         if (page->persistent_page_id == IAF_ID_NEED_REINIT)
